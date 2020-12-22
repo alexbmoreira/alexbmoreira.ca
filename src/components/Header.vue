ca53999e9818be21e348e7ef2c111afc949699db
alexbmoreira <alexbmoreira@gmail.com>
2020-12-22 16:14:23 -0500

    Added linkes to sites and resume

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,21 @@
   <div class="header">
     <div class="words">
       <h1 class="name">Alex Moreira</h1>
-      <h1 class="description">Software Developer • Toronto, ON</h1>
+      <h1 class="description">Software Developer • <i>Toronto, ON</i></h1>
+      <div class="links">
+        <span class="link">
+          <i class="fab fa-github"></i>
+          <p class="link-text">Github</p>
+        </span>
+        <span class="link">
+          <i class="fab fa-linkedin"></i>
+          <p class="link-text">LinkedIn</p>
+        </span>
+        <span class="link">
+          <i class="fas fa-file-pdf"></i>
+          <p class="link-text">Resume</p>
+        </span>
+      </div>
     </div>
     <div class="background"></div>
   </div>
@@ -37,23 +51,39 @@ export default {
 }
 
 .words {
+  color: white; /* text-white */
   display: flex; /* flex */
   flex-direction: column; /* flex-col */
   margin: auto; /* m-auto */
 }
 
 .name {
-  color: white; /* text-white */
   margin: 0 auto; /* mx-auto */
   font-size: 4.5rem;
   line-height: 1; /* text-7xl */
 }
 
 .description {
-  color: white; /* text-white */
   margin: 0 auto; /* mx-auto */
   margin-top: 0.5rem; /* mt-2 */
   font-size: 1.5rem;
   line-height: 2rem; /* text-2xl */
+}
+
+.links {
+  margin: 0 auto; /* mx-auto */
+  margin-top: 1rem; /* mt-4 */
+  font-size: 2.25rem;
+  line-height: 2.5rem; /* text-4xl */
+  justify-content: space-around; /* justify-around */
+  display: flex; /* flex */
+  width: 75%; /* w-9/12 */
+}
+.link {
+  text-align: center; /* text-center */
+}
+.link-text {
+  font-size: 0.875rem;
+  line-height: 1.25rem; /* text-sm */
 }
 </style>
