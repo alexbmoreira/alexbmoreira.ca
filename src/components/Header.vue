e9dbea59fb26a69e9b9ea41f8b92c75dd6a7b217
alexbmoreira <alexbmoreira@gmail.com>
2020-12-22 15:05:56 -0500

    Added background

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
-    <h1>Header</h1>
+    <h1 class="name">Hello</h1>
+    <div class="background"></div>
   </div>
 </template>
 
@@ -16,4 +17,17 @@ export default {
 </script>
 
 <style scoped>
+.header {
+  position: relative;
+}
+
+.background {
+  position: absolute; /* absolute */
+  left: 0; /* left-0 */
+  top: 0; /* top-0 */
+  background-color: black; /* bg-black */
+  width: 100vw; /* w-screen */
+  height: 100vh; /* h-screen */
+  z-index: -10; /* z-n10 */
+}
 </style>
