d3fa5034d1433dfe3288558b585e500911571a3b
alexbmoreira <alexbmoreira@gmail.com>
2020-12-22 12:35:18 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# portfolio
+# alexbmoreira.ca
 
 ## Project setup
 ```
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
