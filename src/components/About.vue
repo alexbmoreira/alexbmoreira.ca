22a341009b65dc56c2aea1f6ba6a49821d96c3ad
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 11:10:55 -0500

    Added headshot

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
-        
+  <div class="bg-gray-700 flex">
+    <div class="m-auto">
+        <div class="flex p-8">
+            <img class="rounded-full w-1/6" src="@/assets/headshot.jpg" alt="">
+            <h1 class="">About Me</h1>
+        </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "About",
-    data() {
-        return {
-            
-        }
-    },
-}
+  name: "About",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style scoped>
-    
 </style>
