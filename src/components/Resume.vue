3c6bf7ae4f89e58249a3b870fcc3e966a4e7cf48
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 16:15:20 -0500

    Added work line

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,14 +1,20 @@
 <template>
-  <div></div>
+	<div class="bg-white flex">
+		<div class="m-auto">
+            <div class="flex p-8 px-64">
+                <span class="font-bold text-3xl border-b-4 border-gray-400">Work Experience</span>
+            </div>
+        </div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Resume",
-  data() {
-    return {};
-  }
-};
+	export default {
+		name: "Resume",
+		data() {
+			return {};
+		},
+	};
 </script>
 
 <style></style>
