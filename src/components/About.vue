678defdb9c317347372e013a1ae9af00e290f902
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 13:12:08 -0500

    Fixed image sizing

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,9 @@
   <div class="bg-gray-800 flex">
     <div class="m-auto">
         <div class="flex p-8 px-64">
-            <img class="rounded-full w-1/6" src="@/assets/headshot.jpg" alt="">
+            <div class="w-1/6">
+                <img class="rounded-full" src="@/assets/headshot.jpg" alt="">
+            </div>
             <div class="pl-6 flex flex-col w-5/6">
                 <h1 class="text-2xl font-bold text-white">About Me</h1>
                 <p class="text-gray-400">A 2021 computer science graduate from the University of Guelph who embraces challenges in programming and has a strong passion for the realm of information technology. Has been programming for over 7 years, in C, Python, Javascript, and Java, with industry experience working in C#, and SQL.</p>
