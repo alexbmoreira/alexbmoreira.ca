0898735ab0aa1fb067724d90c5b437a3d6e56adb
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 14:27:00 -0500

    Added resume component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Header />
     <About />
+    <Resume />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import About from "./components/About";
+import Resume from "./components/Resume";
 
 export default {
   name: "App",
   components: {
     Header,
-    About
+    About,
+    Resume
   }
 };
 </script>
---
+++ src/components/Resume.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Resume",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
