03b26b441d669e054ef0a4831bc1761cca81373d
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 10:35:43 -0500

    Created and imported about component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <About />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import About from "./components/About";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    About
   }
 };
 </script>
---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style scoped>
+    
+</style>
