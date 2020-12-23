3071570dde8a4a5cce6c3c72b38bef4d51bc0ae8
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 11:20:42 -0500

    Added about paragraph

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="bg-gray-700 flex">
+  <div class="bg-gray-800 flex">
     <div class="m-auto">
-        <div class="flex p-8">
+        <div class="flex p-8 px-64">
             <img class="rounded-full w-1/6" src="@/assets/headshot.jpg" alt="">
-            <h1 class="">About Me</h1>
+            <div class="pl-6 flex flex-col w-5/6">
+                <h1 class="text-2xl font-bold text-white">About Me</h1>
+                <p class="text-gray-400">A 2021 computer science graduate from the University of Guelph who embraces challenges in programming and has a strong passion for the realm of information technology. Has been programming for over 7 years, in C, Python, Javascript, and Java, with industry experience working in C#, and SQL.</p>
+            </div>
         </div>
     </div>
   </div>
