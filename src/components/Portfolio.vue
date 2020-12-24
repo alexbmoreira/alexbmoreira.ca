1ed1eb2f9215dc2e389fd2e8778b41b50a77aa8b
alexbmoreira <alexbmoreira@gmail.com>
2020-12-24 17:54:56 -0500

    Added link to github

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,8 +2,10 @@
     <div class="bg-gray-200 flex">
         <div class="mx-auto md:px-6">
             <div class="flex p-8 px-64 xl:px-24 md:px-12 flex-col sm:px-6">
-                <div class="mx-auto mb-8">
+                <div class="mx-auto mb-8 text-center">
                     <span class="font-bold text-3xl border-b-2 border-gray-400">Portfolio</span>
+                    <p class="mt-3">A small collection of notable personal projects.</p>
+                    <p>For more, check out my <a class="text-blue-500 hover:underline" href="https://github.com/alexbmoreira">Github!</a></p>
                 </div>
                 <Project
                 name="alexmoreira.ca"
