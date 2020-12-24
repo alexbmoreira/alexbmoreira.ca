a471497ec6816941c44db37a012c1ac5345a3a6c
alexbmoreira <alexbmoreira@gmail.com>
2020-12-24 11:21:27 -0500

    Added portfolio section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header />
     <About />
     <Resume />
+    <Portfolio />
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
 
 export default {
   name: "App",
   components: {
     Header,
     About,
-    Resume
+    Resume,
+    Portfolio
   }
 };
 </script>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-gray-200 flex">
+        <div class="mx-auto">
+            <div class="flex p-8 px-64 xl:px-24 md:px-12 flex-col sm:px-6">
+                <div class="mx-auto mb-8">
+                    <span class="font-bold text-3xl border-b-2 border-gray-400">Portfolio</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Portfolio",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
