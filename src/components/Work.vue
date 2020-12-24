e1d73c581f2f9e8d2dc692af85e91fcd371ee614
alexbmoreira <alexbmoreira@gmail.com>
2020-12-23 22:23:33 -0500

    Dynamically grab image paths

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col mb-16">
         <div class="flex mb-2">
             <div class="w-1/12 sm:w-1/6 mr-5">
-                <img class="rounded-full" :src="getCompanyLogo(image)" alt="">
+                <img class="rounded-full" :src="getCompanyLogo()" alt="">
             </div>
             <div class="flex flex-col">
                 <span class="text-2xl font-bold">{{ company }}</span>
@@ -32,8 +32,8 @@ export default {
         duties: Array,
     },
     methods: {
-        getCompanyLogo(image) {
-            return require(`@/assets/images/company_logos/${image}`)
+        getCompanyLogo() {
+            return require(`@/assets/images/company_logos/${this.image}`)
         }
     },
 }
