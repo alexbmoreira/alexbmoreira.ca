32e7aeee28fc003f5e7d780292840edf7aa8518e
alexbmoreira <alexbmoreira@gmail.com>
2020-12-24 17:33:09 -0500

    Fixed centering

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="bg-white flex">
 		<div class="m-auto">
-            <div class="flex p-8 px-64 xl:px-24 md:px-12 flex-col sm:px-6">
+            <div class="flex flex-col p-8 px-64 xl:px-24 md:px-12 sm:px-6">
                 <div class="mx-auto mb-8">
                     <span class="font-bold text-3xl border-b-2 border-gray-400">Experience</span>
                 </div>
-                <div class="pl-6">
+                <div class="pl-6 md:px-6 md:mx-auto">
                     <Term
                     org="Superior Glove"
                     role="Software Developer (.NET)"
