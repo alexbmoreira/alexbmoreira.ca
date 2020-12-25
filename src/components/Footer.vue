f50022fabae9d039860a61fe4b8a5976784c4618
alexbmoreira <alexbmoreira@gmail.com>
2020-12-25 15:44:58 -0500

    Changed links in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="relative flex bg-black p-8">
     <div class="text-gray-700 flex flex-col m-auto">
-      <div class="mx-auto mt-2 text-2xl justify-around space-x-16 flex w-9/12 md:text-3xl">
+      <div class="text-center">
+        <p>Alex Moreira</p>
+      </div>
+      <div class="mx-auto mt-2 text-2xl justify-around space-x-16 flex md:text-3xl">
         <a href="https://github.com/alexbmoreira" class="text-center transition duration-400 ease-in-out hover:text-white">
           <i class="fab fa-github"></i>
         </a>
@@ -11,11 +14,12 @@
         <a href="" class="text-center transition duration-400 ease-in-out hover:text-white">
           <i class="fas fa-file-pdf"></i>
         </a>
-      </div>
-      <div class="text-center mt-3">
-        <p>Alex Moreira</p>
-        <a href="mailto:alexbmoreira@gmail.com"><p class="transition duration-400 ease-in-out hover:text-white">alexbmoreira@gmail.com</p></a>
-        <a href="tel:+14164336886"><p class="transition duration-400 ease-in-out hover:text-white">(416) 433-6886</p></a>
+        <a href="mailto:alexbmoreira@gmail.com" class="text-center transition duration-400 ease-in-out hover:text-white">
+          <i class="fas fa-envelope"></i>
+        </a>
+        <a href="tel:+14164336886" class="text-center transition duration-400 ease-in-out hover:text-white">
+          <i class="fas fa-phone-alt"></i>
+        </a>
       </div>
     </div>
   </div>
