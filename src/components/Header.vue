1a128c2bb0937d3227f605bc3d64294bc7fffa9d
alexbmoreira <alexbmoreira@gmail.com>
2020-12-25 13:50:53 -0500

    Minor restyling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="relative flex bg-black w-full h-screen">
-    <div class="text-white flex flex-col m-auto">
+    <div class="text-white flex flex-col m-auto space-y-6">
       <h1 class="mx-auto text-6xl md:text-5xl">Alex Moreira</h1>
       <h1 class="mx-auto text-2xl md:text-xl">Software Developer • <i>Toronto, ON</i></h1>
-      <div class="mx-auto mt-2 text-4xl justify-around flex w-9/12 md:text-3xl">
-        <a href="https://github.com/alexbmoreira" class="text-center transition duration-400 ease-in-out hover:text-blue-400">
+      <div class="mx-auto text-4xl justify-around flex w-9/12 md:text-3xl">
+        <a href="https://github.com/alexbmoreira" class="text-center transition duration-400 ease-in-out hover:text-blue-600">
           <i class="fab fa-github"></i>
           <p class="text-xs">Github</p>
         </a>
-        <a href="https://www.linkedin.com/in/alexbmoreira" class="text-center transition duration-400 ease-in-out hover:text-blue-400">
+        <a href="https://www.linkedin.com/in/alexbmoreira" class="text-center transition duration-400 ease-in-out hover:text-blue-600">
           <i class="fab fa-linkedin"></i>
           <p class="text-xs">LinkedIn</p>
         </a>
-        <a href="" class="text-center transition duration-400 ease-in-out hover:text-blue-400">
+        <a href="" class="text-center transition duration-400 ease-in-out hover:text-blue-600">
           <i class="fas fa-file-pdf"></i>
           <p class="text-xs">Resume</p>
         </a>
