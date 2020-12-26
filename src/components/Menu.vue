f1679c846e24fc350d79dbec8ceef317e87c9d77
alexbmoreira <alexbmoreira@gmail.com>
2020-12-26 00:17:15 -0500

    Added menu transition

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <div :class="{'scale-100': expand}" class="transform origin-menu rounded bg-black fixed top-4 left-4 p-3 scale-0 sm:p-2 sm:top-2 sm:left-2 transition duration-500 ease-in-out">
-            <div class="flex flex-col space-y-2">
+        <div :class="{'scale-100': expand}" class="transform origin-menu rounded bg-black fixed top-4 left-4 p-3 scale-0 sm:p-2 sm:top-2 sm:left-2 transition duration-300 ease-in-out">
+            <div class="flex flex-col">
                 <div class="flex text-white h-full cursor-pointer mb-3 text-2xl sm:text-base" @click="clickMenu()">
                     <i class="fas fa-bars"></i>
                 </div>
-                <a href="#about"><span class="cursor-pointer text-white text-lg hover:text-blue-600">About</span></a>
-                <a href="#experience"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Experience</span></a>
-                <a href="#education"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Education</span></a>
-                <a href="#portfolio"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Portfolio</span></a>
+                <div :class="{'opacity-100': expand}" class="flex flex-col space-y-2 opacity-0 transition delay-100 duration-300 ease-in-out">
+                    <a href="#about"><span class="cursor-pointer text-white text-lg hover:text-blue-600">About</span></a>
+                    <a href="#experience"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Experience</span></a>
+                    <a href="#education"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Education</span></a>
+                    <a href="#portfolio"><span class="cursor-pointer text-white text-lg hover:text-blue-600">Portfolio</span></a>
+                </div>
             </div>
         </div>
         <div class="rounded-full bg-black fixed top-4 left-4 w-12 h-12 sm:w-8 sm:h-8 sm:top-2 sm:left-2">
