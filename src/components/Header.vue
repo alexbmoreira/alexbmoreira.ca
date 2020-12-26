e6af063661e0eb71a29e1ba23f8bb69459d3c691
alexbmoreira <alexbmoreira@gmail.com>
2020-12-26 12:17:31 -0500

    Removed commented styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,57 +38,5 @@ export default {
 };
 </script>
 
-<!-- <style scoped>
-.header {
-  position: relative; /* relative */
-  display: flex; /* flex */
-  height: 100vh; /* h-screen */
-}
-
-.background {
-  position: absolute; /* absolute */
-  left: 0; /* left-0 */
-  top: 0; /* top-0 */
-  background-color: black; /* bg-black */
-  width: 100vw; /* w-screen */
-  height: 100vh; /* h-screen */
-  z-index: -10; /* z-n10 */
-}
-
-.words {
-  color: white; /* text-white */
-  display: flex; /* flex */
-  flex-direction: column; /* flex-col */
-  margin: auto; /* m-auto */
-}
-
-.name {
-  margin: 0 auto; /* mx-auto */
-  font-size: 3.75rem;
-  line-height: 1; /* text-6xl */
-}
-
-.description {
-  margin: 0 auto; /* mx-auto */
-  margin-top: 0.5rem; /* mt-2 */
-  font-size: 1.5rem;
-  line-height: 2rem; /* text-2xl */
-}
-
-.links {
-  margin: 0 auto; /* mx-auto */
-  margin-top: 1rem; /* mt-4 */
-  font-size: 2.25rem;
-  line-height: 2.5rem; /* text-4xl */
-  justify-content: space-around; /* justify-around */
-  display: flex; /* flex */
-  width: 75%; /* w-9/12 */
-}
-.link {
-  text-align: center; /* text-center */
-}
-.link-text {
-  font-size: 0.875rem;
-  line-height: 1.25rem; /* text-sm */
-}
-</style> -->
+<style scoped>
+</style>
