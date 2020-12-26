52945b4d763a3dac820c0f0abd00af891bd1d539
alexbmoreira <alexbmoreira@gmail.com>
2020-12-25 23:32:11 -0500

    Resized menu for smaller screens

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="rounded-full bg-blue-600 fixed top-4 left-4 w-12 h-12">
-        <div class="flex justify-center text-white items-center h-full text-2xl">
+    <div class="rounded-full bg-blue-600 fixed top-4 left-4 w-12 h-12 sm:w-8 sm:h-8 sm:top-2 sm:left-2">
+        <div class="flex justify-center text-white items-center h-full text-2xl sm:text-base">
             <i class="fas fa-bars"></i>
         </div>
     </div>
