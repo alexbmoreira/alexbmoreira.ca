a8a59fdc66915081167aeb53949c48462b7d1f9b
alexbmoreira <alexbmoreira@gmail.com>
2020-12-27 18:38:17 -0500

    Added icons to navbar and smaller font classes to config

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,22 @@
 <template>
-    <div class="absolute bottom-0 flex w-full">
-        <div class="flex p-6 px-8 text-white text-lg font-bold mx-auto space-x-36 lg:space-x-24 md:space-x-12 sm:text-base sm:space-x-6 sm:px-3">
-            <a href="#about"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">About</span></a>
-            <a href="#experience"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Experience</span></a>
-            <a href="#education"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Education</span></a>
-            <a href="#portfolio"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Portfolio</span></a>
+    <div class="absolute bottom-0 flex w-full pb-5 sm:pb-2">
+        <div class="grid grid-cols-4 text-white text-lg font-bold w-full text-center sm:text-2xs">
+            <div class="flex flex-col">
+                <span class="invisible sm:visible sm:text-xl"><i class="fas fa-info-circle"></i></span>
+                <a href="#about"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">About</span></a>
+            </div>
+            <div class="flex flex-col">
+                <span class="invisible sm:visible sm:text-xl"><i class="fas fa-briefcase"></i></span>
+                <a href="#experience"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Experience</span></a>
+            </div>
+            <div class="flex flex-col">
+                <span class="invisible sm:visible sm:text-xl"><i class="fas fa-graduation-cap"></i></span>
+                <a href="#education"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Education</span></a>
+            </div>
+            <div class="flex flex-col">
+                <span class="invisible sm:visible sm:text-xl"><i class="fas fa-code"></i></span>
+                <a href="#portfolio"><span class="cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-blue-600">Portfolio</span></a>
+            </div>
         </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,22 @@ module.exports = {
     fontFamily: {
       'quicksand': ['Quicksand', 'sans-serif']
     },
+    fontSize: {
+      '4xs': '0.25rem',
+      '3xs': '0.375rem',
+      '2xs': '0.5rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       backgroundImage: theme => ({
        'space': "url('../images/header_bg.jpg')",
