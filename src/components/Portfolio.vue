fb3b05f5e7c74b4d9277ce7eaef8f379232acd47
alexbmoreira <alexbmoreira@gmail.com>
2020-12-28 18:26:19 -0500

    Added links to projects that are deployed

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -17,39 +17,31 @@
           </p>
         </div>
         <Project
-          name="alexbmoreira.ca"
-          desc="This site was designed and built myself using Vue.js and Tailwind. I made it to display the skills and experience on my resume in a more appealing way, as well as to highlight notable personal projects that aren't listed on there."
-          link="alexbmoreira.ca"
-          :stack="[
-            { name: 'vuejs', color: 'text-green-500' },
-            { name: 'css3-alt', color: 'text-blue-400' }
-          ]"
-          image="alexbmoreira.jpg"
-        />
+            name="alexbmoreira.ca"
+            desc="This site was designed and built myself using Vue.js and Tailwind. I made it to display the skills and experience on my resume in a more appealing way, as well as to highlight notable personal projects that aren't listed on there."
+            github_link="alexbmoreira.ca"
+            :stack="[{'name': 'vuejs', 'color': 'text-green-500'},
+            {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+            image="alexbmoreira.jpg" />
         <Project
-          name="Filler"
-          desc="A clone of the iOS GamePigeon Filler game. My sister loves playing this game with her friends over iMessage, so I made a version online that she could play when none of her friends can."
-          link="filler"
-          :stack="[
-            { name: 'python', color: 'text-blue-700' },
-            { name: 'js-square', color: 'text-yellow-300' },
-            { name: 'html5', color: 'text-yellow-600' },
-            { name: 'css3-alt', color: 'text-blue-400' }
-          ]"
-          image="filler.jpg"
-        />
+            name="Filler"
+            desc="A clone of the iOS GamePigeon Filler game. My sister loves playing this game with her friends over iMessage, so I made a version online that she could play when none of her friends can."
+            github_link="filler"
+            website="https://playfiller.herokuapp.com/"
+            :stack="[{'name': 'python', 'color': 'text-blue-700'},
+            {'name': 'js-square', 'color': 'text-yellow-300'},
+            {'name': 'html5', 'color': 'text-yellow-600'},
+            {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+            image="filler.jpg" />
         <Project
-          name="Namaste"
-          desc="A simple yoga sequence builder put together over a weekend for the 2020 Winter Hacklympics hackathon. Coded with a partner in Django and Vue.js."
-          link="yoga-app"
-          devpost="https://devpost.com/software/namaste-q1j6gk"
-          :stack="[
-            { name: 'python', color: 'text-blue-700' },
-            { name: 'vuejs', color: 'text-green-500' },
-            { name: 'css3-alt', color: 'text-blue-400' }
-          ]"
-          image="namaste.jpg"
-        />
+            name="Namaste"
+            desc="A simple yoga sequence builder put together over a weekend for the 2020 Winter Hacklympics hackathon. Coded with a partner in Django and Vue.js."
+            github_link="yoga-app"
+            devpost="https://devpost.com/software/namaste-q1j6gk"
+            :stack="[{'name': 'python', 'color': 'text-blue-700'},
+            {'name': 'vuejs', 'color': 'text-green-500'},
+            {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+            image="namaste.jpg" />
       </div>
     </div>
   </div>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,60 +1,57 @@
 <template>
-  <div class="flex mb-16 sm:flex-col">
-    <div class="w-1/3 mr-6 md:w-1/2 md:mb-2 sm:w-full">
-      <img class="shadow-lg" :src="getProjectImage()" alt="" />
-    </div>
-    <div class="flex flex-col w-2/3 md:w-1/2 sm:w-full">
-      <div class="flex items-center justify-between">
-        <span class="text-2xl font-bold mb-3">{{ name }}</span>
-        <div class="flex space-x-3">
-          <a v-if="devpost" :href="devpost" class="text-2xl">
-            <img src="@/assets/images/devpost.png" class="h-6 align-middle" />
-          </a>
-          <a
-            :href="getGithubLink()"
-            class="text-2xl transition duration-400 ease-in-out hover:text-blue-600"
-          >
-            <i class="fab fa-github align-top"></i>
-          </a>
-        </div>
-      </div>
-      <p class="text-justify mb-3">{{ desc }}</p>
-      <div class="flex space-x-3">
-        <div
-          v-for="(tech, index) in stack"
-          :key="index"
-          :class="`text-3xl ${tech.color}`"
-        >
-          <i :class="`fab fa-${tech.name}`"></i>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="flex mb-16 sm:flex-col">
+		<div class="w-1/3 mr-6 md:w-1/2 md:mb-2 sm:w-full">
+			<img class="shadow-lg" :src="getProjectImage()" alt="" />
+		</div>
+		<div class="flex flex-col w-2/3 md:w-1/2 sm:w-full">
+			<div class="flex items-center justify-between">
+				<span class="text-2xl font-bold mb-3">{{ name }}</span>
+				<div class="flex space-x-3">
+					<a v-if="devpost" :href="devpost" class="text-2xl">
+						<img src="@/assets/images/devpost.png" class="h-6 align-middle" />
+					</a>
+					<a v-if="website" :href="website" class="text-2xl transition duration-400 ease-in-out hover:text-blue-600">
+						<i class="fas fa-link align-top"></i>
+					</a>
+					<a :href="getGithubLink()" class="text-2xl transition duration-400 ease-in-out hover:text-blue-600">
+						<i class="fab fa-github align-top"></i>
+					</a>
+				</div>
+			</div>
+			<p class="text-justify mb-3">{{ desc }}</p>
+			<div class="flex space-x-3">
+				<div v-for="(tech, index) in stack" :key="index" :class="`text-3xl ${tech.color}`">
+					<i :class="`fab fa-${tech.name}`"></i>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Project",
-  data() {
-    return {};
-  },
-  props: {
-    name: String,
-    desc: String,
-    link: String,
-    stack: Array,
-    devpost: String,
-    image: String
-  },
-  methods: {
-    getGithubLink() {
-      return `https://github.com/alexbmoreira/${this.link}`;
-    },
-    getProjectImage() {
-      return require(`@/assets/images/project_images/${this.image}`);
-    }
-  }
-};
+	export default {
+		name: "Project",
+		data() {
+			return {};
+		},
+		props: {
+			name: String,
+			desc: String,
+            github_link: String,
+            website: String,
+			stack: Array,
+			devpost: String,
+			image: String,
+		},
+		methods: {
+			getGithubLink() {
+				return `https://github.com/alexbmoreira/${this.github_link}`;
+			},
+			getProjectImage() {
+				return require(`@/assets/images/project_images/${this.image}`);
+			},
+		},
+	};
 </script>
 
 <style></style>
