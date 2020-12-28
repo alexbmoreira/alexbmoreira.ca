e96ebf9aaf00f36e4407b22de6a58d4b96ed90a7
alexbmoreira <alexbmoreira@gmail.com>
2020-12-28 18:27:36 -0500

    Added links to projects that are deployed

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
   <div id="portfolio" class="bg-gray-200 flex">
     <div class="mx-auto md:px-6">
       <div class="flex p-8 px-64 xl:px-24 md:px-12 flex-col sm:px-6">
@@ -15,6 +16,43 @@
               >Github!</a
             >
           </p>
+=======
+    <div id="portfolio" class="bg-gray-200 flex">
+        <div class="mx-auto md:px-6">
+            <div class="flex p-8 px-64 xl:px-24 md:px-12 flex-col sm:px-6">
+                <div class="mx-auto mb-8 text-center">
+                    <span class="font-bold text-3xl border-b-2 border-gray-400">Portfolio</span>
+                    <p class="mt-3">A small collection of notable personal projects.</p>
+                    <p>For more, check out my <a class="text-blue-500 hover:underline" href="https://github.com/alexbmoreira">Github!</a></p>
+                </div>
+                <Project
+                name="alexbmoreira.ca"
+                desc="This site was designed and built myself using Vue.js and Tailwind. I made it to display the skills and experience on my resume in a more appealing way, as well as to highlight notable personal projects that aren't listed on there."
+                git_link="alexbmoreira.ca"
+                :stack="[{'name': 'vuejs', 'color': 'text-green-500'},
+                {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+                image="alexbmoreira.jpg" />
+                <Project
+                name="Filler"
+                desc="A clone of the iOS GamePigeon Filler game. My sister loves playing this game with her friends over iMessage, so I made a version online that she could play when none of her friends can."
+                git_link="filler"
+                website="https://playfiller.herokuapp.com"
+                :stack="[{'name': 'python', 'color': 'text-blue-700'},
+                {'name': 'js-square', 'color': 'text-yellow-300'},
+                {'name': 'html5', 'color': 'text-yellow-600'},
+                {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+                image="filler.jpg" />
+                <Project
+                name="Namaste"
+                desc="A simple yoga sequence builder put together over a weekend for the 2020 Winter Hacklympics hackathon. Coded with a partner in Django and Vue.js."
+                git_link="yoga-app"
+                devpost="https://devpost.com/software/namaste-q1j6gk"
+                :stack="[{'name': 'python', 'color': 'text-blue-700'},
+                {'name': 'vuejs', 'color': 'text-green-500'},
+                {'name': 'css3-alt', 'color': 'text-blue-400'}]"
+                image="namaste.jpg" />
+            </div>
+>>>>>>> 234f8c69444bab57de2793d92e732880128bfc59
         </div>
         <Project
             name="alexbmoreira.ca"
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
 	<div class="flex mb-16 sm:flex-col">
 		<div class="w-1/3 mr-6 md:w-1/2 md:mb-2 sm:w-full">
 			<img class="shadow-lg" :src="getProjectImage()" alt="" />
@@ -52,6 +53,61 @@
 			},
 		},
 	};
+=======
+    <div class="flex mb-16 sm:flex-col">
+        <div class="w-1/3 mr-6 md:w-1/2 md:mb-2 sm:w-full">
+            <img class="shadow-lg" :src="getProjectImage()" alt="">
+        </div>
+        <div class="flex flex-col w-2/3 md:w-1/2 sm:w-full">
+            <div class="flex items-center justify-between">
+                <span class="text-2xl font-bold mb-3">{{ name }}</span>
+                <div class="flex space-x-3">
+                    <a v-if="devpost" :href="devpost" class="text-2xl">
+                        <img src="@/assets/images/devpost.png" class="h-6 align-middle">
+                    </a>
+                    <a v-if="website" :href="website" class="text-2xl transition duration-400 ease-in-out hover:text-blue-600 cursor-pointer">
+                        <i class="fas fa-link align-top"></i>
+                    </a>
+                    <a :href="getGithubLink()" class="text-2xl transition duration-400 ease-in-out hover:text-blue-600">
+                        <i class="fab fa-github align-top"></i>
+                    </a>
+                </div>
+            </div>
+            <p class="text-justify mb-3">{{ desc }}</p>
+            <div class="flex space-x-3">
+                <div v-for="(tech, index) in stack" :key="index" :class="`text-3xl ${tech.color}`">
+                    <i :class="`fab fa-${tech.name}`"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Project",
+    data() {
+        return {}
+    },
+    props: {
+        name: String,
+        desc: String,
+        git_link: String,
+        website: String,
+        stack: Array,
+        devpost: String,
+        image: String
+    },
+    methods: {
+        getGithubLink() {
+            return `https://github.com/alexbmoreira/${this.git_link}`
+        },
+        getProjectImage() {
+            return require(`@/assets/images/project_images/${this.image}`)
+        }
+    },
+}
+>>>>>>> 234f8c69444bab57de2793d92e732880128bfc59
 </script>
 
 <style></style>
