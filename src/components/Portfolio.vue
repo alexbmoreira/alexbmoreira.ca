0590fcc312f8694ea936888b8fb5570da7b81f6b
alexbmoreira <alexbmoreira@gmail.com>
2021-02-02 13:04:09 -0500

    Added movie app

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -34,14 +34,15 @@
             {'name': 'css3', 'paths': 4}]"
             image="filler.jpg" />
         <Project
-            name="Movie Matcher"
-            desc="A work-in-progress webapp created by myself and a friend with which one can create a watchlist, add friends, and consult the app for what to watch with those friends. The app will provide a swiping interface--not unlike that of most dating apps--that will cycle through the watchlists of you and a friend and notify both when you match. Gone are the days of spending hours deciding on a movie! Once it's launched, that is..."
+            name="Movie Matching App"
+            desc="A work-in-progress web app that will allow users to add their friends, as well as search for and add movies to a watchlist. The app will decide what to watch with their friends based on matches from a swipeable UI not unlike most dating apps."
             github_link="movie-match"
             :stack="[{'name': 'python', 'paths': 2},
             {'name': 'django', 'paths': 0},
             {'name': 'vue', 'paths': 2},
             {'name': 'tailwindcss', 'paths': 0},
-            {'name': 'postgresql', 'paths': 4}]" />
+            {'name': 'postgresql', 'paths': 4}]"
+            image="moviematch.jpg"  />
         <Project
             name="Namaste"
             desc="A simple yoga sequence builder put together over a weekend for the 2020 Winter Hacklympics hackathon. Coded with a partner in Django and Vue.js."
