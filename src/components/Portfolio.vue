00fcd6eb8699a1f76976f3cc49656111be6f249f
alexbmoreira <alexbmoreira@gmail.com>
2021-07-17 21:47:15 -0400

    Made 'api' for projects and work terms

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -18,125 +18,16 @@
           </p>
         </div>
         <Project
-          name="alexbmoreira.ca"
-          desc="This site was designed and built myself using Vue.js and Tailwind. I made it to display the skills and experience on my resume in a more appealing way, as well as to highlight notable personal projects that aren't listed on there."
-          github_link="alexbmoreira.ca"
-          :stack="[
-            {
-              name: 'vue',
-              paths: 2
-            },
-            {
-              name: 'tailwindcss',
-              paths: 0
-            }
-          ]"
-          :deploy="[
-            {
-              name: 'firebase',
-              paths: 4
-            }
-          ]"
-          image="alexbmoreira.jpg"
-        />
-        <Project
-          name="Filler"
-          desc="Web app clone of the GamePigeon iMessage game made with Python and Flask. It's my sister's favorite game and she wanted a version to play when her friends aren't available. It's played against a computer which will algorithmically make the best move each time."
-          github_link="filler"
-          website="https://playfiller.herokuapp.com/"
-          :stack="[
-            {
-              name: 'python',
-              paths: 2
-            },
-            {
-              name: 'javascript',
-              paths: 2
-            },
-            {
-              name: 'html5',
-              paths: 4
-            },
-            {
-              name: 'css3',
-              paths: 4
-            }
-          ]"
-          :deploy="[
-            {
-              name: 'heroku',
-              paths: 2
-            }
-          ]"
-          image="filler.jpg"
-        />
-        <Project
-          name="Movie Matching App"
-          desc="A work-in-progress web app that will allow users to add their friends, as well as search for and add movies to a watchlist. The app will decide what to watch with their friends based on matches from a swipeable UI not unlike most dating apps."
-          github_link="movie-match"
-          :stack="[
-            {
-              name: 'python',
-              paths: 2
-            },
-            {
-              name: 'django',
-              paths: 0
-            },
-            {
-              name: 'postgresql',
-              paths: 4
-            },
-            {
-              name: 'vue',
-              paths: 2
-            },
-            {
-              name: 'tailwindcss',
-              paths: 0
-            }
-          ]"
-          image="moviematch.jpg"
-        />
-        <Project
-          name="Namaste"
-          desc="A simple yoga sequence builder put together over a weekend for the 2020 Winter Hacklympics hackathon. Coded with a partner in Django and Vue.js."
-          github_link="yoga-app"
-          website="https://namaste-22f27.web.app/"
-          devpost="https://devpost.com/software/namaste-q1j6gk"
-          :stack="[
-            {
-              name: 'python',
-              paths: 2
-            },
-            {
-              name: 'django',
-              paths: 0
-            },
-            {
-              name: 'postgresql',
-              paths: 4
-            },
-            {
-              name: 'vue',
-              paths: 2
-            },
-            {
-              name: 'css3',
-              paths: 4
-            }
-          ]"
-          :deploy="[
-            {
-              name: 'heroku',
-              paths: 2
-            },
-            {
-              name: 'firebase',
-              paths: 4
-            }
-          ]"
-          image="namaste.jpg"
+          v-for="(project, index) in projects"
+          :name="project.name"
+          :desc="project.desc"
+          :github_link="project.github_link"
+          :webiste="project.webiste"
+          :devpost="project.devpost"
+          :stack="project.stack"
+          :deploy="project.deploy"
+          :image="project.image"
+          :key="index"
         />
       </div>
     </div>
@@ -145,6 +36,7 @@
 
 <script>
 import Project from "./Project";
+import projects from "../api/projects";
 
 export default {
   name: "Portfolio",
@@ -152,7 +44,9 @@ export default {
     Project
   },
   data() {
-    return {};
+    return {
+      projects
+    };
   }
 };
 </script>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -9,45 +9,13 @@
         </div>
         <div class="pl-6 md:px-6 md:mx-auto">
           <Term
-            org="Collage HR"
-            role="Junior Software Developer"
-            time="May. 2021 – Present"
-            image="collage_hr.png"
-            :notes="[]"
-          />
-          <Term
-            org="Superior Glove"
-            role="Software Developer Co-op (.NET)"
-            time="May. 2020 – Aug. 2020"
-            image="superior_glove.png"
-            :notes="[
-              'Took the lead on a 4-month project creating a custom MRP solution in C# using WPF and other .NET libraries and frameworks.',
-              'Wrote complex SQL procedures with Microsoft SQL Server.',
-              'Managed help desk tickets for over 200 employees using Jira.',
-              'Engaged in daily scrum meetings with a development team of 7 people to discuss project development.'
-            ]"
-          />
-          <Term
-            org="Welbilt Inc."
-            role="Buyer/Planner Intern"
-            time="May. 2019 – Aug. 2019"
-            image="welbilt.png"
-            :notes="[
-              'Automated supplier scheduling and increased efficiency of the purchasing department by 20%.',
-              'Communicated daily with suppliers about expediting orders and advising on past-dues, as well as sending out steel and cardboard consignment releases.',
-              'Scheduled welding and production lines using QAD and Excel.'
-            ]"
-          />
-          <Term
-            org="kubik"
-            role="Warehouse Associate"
-            time="May. 2018 – Jul. 2018"
-            image="kubik.png"
-            :notes="[
-              'Worked with a team constructing displays for retail environments, trade shows, and exhibits.',
-              'Assisted in fulfilling the visions of clients such as Best Buy Canada, Mars Wrigley, and the Royal Alberta Museum.',
-              'Gained valuable experience in a professional work environment, where teamwork was a top focus.'
-            ]"
+            v-for="(term, index) in work_terms"
+            :org="term.org"
+            :role="term.role"
+            :time="term.time"
+            :image="term.image"
+            :notes="term.notes"
+            :key="index"
           />
         </div>
         <div id="education" class="mx-auto py-8">
@@ -59,7 +27,7 @@
           <Term
             org="University of Guelph"
             role="Bachelor of Computer Science, Honors"
-            time="Graduating Apr. 2021"
+            time="Sept. 2017 Apr. 2021"
             image="uoguelph.jpg"
             :notes="['Emphasis in Mathematical Science']"
           />
@@ -71,6 +39,7 @@
 
 <script>
 import Term from "./Term";
+import work_terms from "../api/work_terms";
 
 export default {
   name: "Resume",
@@ -78,7 +47,9 @@ export default {
     Term
   },
   data() {
-    return {};
+    return {
+      work_terms
+    };
   }
 };
 </script>
