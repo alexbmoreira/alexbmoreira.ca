1a39fac09c70e5a24ce2dad727431d3b864b99c9
Alex Moreira <alexbmoreira@gmail.com>
2024-11-11 19:32:40 -0500

    Fix icon colors (#20)
    * wip
    
    * fix

--- src/api/projects.js
+++ src/api/projects.js
@@ -14,8 +14,18 @@ const projects = [
       "A personal cookbook site I created with Rails and React, designed as my go-to platform for viewing and managing my collection of personal recipes. I can create and modify recipies, and anyone can sign up to add private notes to each recipe, making it easy to jot down tweaks or tips each time I make them. Best viewed on mobile.",
     github_link: "cookbook-site",
     website: "https://twoscompanycookbook.com/",
-    stack: ["ruby", "rails", "react", "postgresql", "tailwind"],
-    deploy: ["heroku", "firebase", "google-cloud"],
+    stack: [
+      { class: "fa-ruby", color: "text-brand-ruby" },
+      { class: "fa-rails", color: "text-brand-rails" },
+      { class: "fa-react", color: "text-brand-react" },
+      { class: "fa-postgresql", color: "text-brand-postgresql" },
+      { class: "fa-tailwind", color: "text-brand-tailwind" }
+    ],
+    deploy: [
+      { class: "fa-heroku", color: "text-brand-heroku" },
+      { class: "fa-firebase", color: "text-brand-firebase" },
+      { class: "fa-google-cloud", color: "text-brand-google-cloud" }
+    ],
     image: "twos-company-cookbook.jpg"
   },
   {
@@ -23,16 +33,19 @@ const projects = [
     desc:
       "Web app clone of the GamePigeon iMessage game made with Python and Flask. It's my sister's favorite game and she wanted a version to play when her friends aren't available. It's played against a computer which will algorithmically make the best move each time.",
     github_link: "filler",
-    stack: ["python"],
-    deploy: ["heroku"],
+    stack: [{ class: "fa-python", color: "text-brand-python" }],
+    deploy: [{ class: "fa-heroku", color: "text-brand-heroku" }],
     image: "filler.jpg"
   },
   {
     name: "Forj Custom Homes",
     desc:
       "The official website for Forj Custom Homes, a custom home project management business based out of Carlise, ON.",
-    stack: ["react"],
-    deploy: ["firebase", "google-cloud"],
+    stack: [{ class: "fa-react", color: "text-brand-react" }],
+    deploy: [
+      { class: "fa-firebase", color: "text-brand-firebase" },
+      { class: "fa-google-cloud", color: "text-brand-google-cloud" }
+    ],
     image: "forj-custom-homes.jpg"
   },
   {
@@ -40,7 +53,12 @@ const projects = [
     desc:
       "A social film app that transforms the often daunting task of deciding what movie to watch into a seamless process. Users curate personal watchlists and, using a Tinder-style interface, can match with friends on a mutual film choice for their next movie night.",
     github_link: "movie-match",
-    stack: ["ruby", "rails", "react", "postgresql", "tailwind"],
+    stack: [
+      { class: "fa-ruby", color: "text-brand-ruby" },
+      { class: "fa-rails", color: "text-brand-rails" },
+      { class: "fa-react", color: "text-brand-react" },
+      { class: "fa-postgresql", color: "text-brand-postgresql" }
+    ],
     image: "moviematch.jpg"
   }
 ];
