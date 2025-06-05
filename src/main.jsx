0a7f257b35429ed70a06bc1a5b6ab7c59a86bdb2
Alex Moreira <alexbmoreira@gmail.com>
2025-06-04 21:56:22 -0400

    CLose menu on scroll and scroll smoothly (#26)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@styles/index.css'
 import '@styles/tailwind.css'
 import '@styles/fonts.css'
 import Root from './Root'
