6ef32fd52b38048581bff8270a438b9114c0edcb
Alex Moreira <alexbmoreira@gmail.com>
2025-06-04 21:55:49 -0400

    CLose menu on scroll and scroll smoothly

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,5 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Header } from '@components';
 import { AnimatePresence, motion } from "motion/react"
 
 const MenuItem = ({text, href, onClick}) => {
@@ -10,26 +9,35 @@ const MenuItem = ({text, href, onClick}) => {
 const Menu = ({}) => {
   const [expanded, setExpanded] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setExpanded(false);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div>
       <div
-        className='flex bg-midnight text-snow fixed top-4 left-4 justify-center items-center cursor-pointer w-10 h-10 text-lg rounded-full'
+        className='flex bg-midnight text-snow fixed top-4 left-4 justify-center items-center cursor-pointer w-10 h-10 text-lg rounded-full z-10'
         onClick={() => setExpanded(!expanded)}
       >
         <FontAwesomeIcon icon='fa-solid fa-bars'/>
       </div>
       <AnimatePresence mode='wait'>
         {expanded && <motion.div
-          className='flex flex-col bg-midnight text-snow p-4 space-y-2 rounded-2xl fixed left-9 top-16 origin-top-left'
+          className='flex flex-col bg-midnight text-snow p-4 space-y-2 rounded-2xl fixed origin-top-left'
           layout
           transition={{
               type: "spring",
               visualDuration: 0.2,
               bounce: 0.2,
           }}
-          initial={{ left: '2.25rem', scale: 0 }}
-          animate={{ left: '1rem', scale: 1 }}
-          exit={{ left: '2.25rem', scale: 0 }}
+          initial={{ left: '2.25rem', top: '2.25rem', scale: 0 }}
+          animate={{ left: '1rem', top: '4rem', scale: 1 }}
+          exit={{ left: '2.25rem', top: '2.25rem', scale: 0 }}
         >
           <MenuItem text='About' href='#about' onClick={() => setExpanded(false)}/>
           <MenuItem text='Experience' href='#experience' onClick={() => setExpanded(false)}/>
